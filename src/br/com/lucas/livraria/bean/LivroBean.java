5782d11c54a91fdc7eb874cb545514a63efacf6d
Lucas Oliveira <lcsd.lucas@gmail.com>
2020-06-22 22:48:16 -0300

    primeiro commit

---
+++ src/br/com/lucas/livraria/bean/LivroBean.java
@@ -0,0 +1,21 @@
+package br.com.lucas.livraria.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LivroBean {
+
+	private Livro livro = new Livro();
+
+	public void gravar() {
+		System.out.println(livro.getTitulo());
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+}
